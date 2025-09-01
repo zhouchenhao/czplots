ec5ac41cc3ed71b75d9eb565be0131efd15fd92d
zhouchenhao <cheer_hao@hotmail.com>
2025-09-01 14:58:38 +1000

    Update vignette

--- tests/testthat/test-countpercluster.R
+++ tests/testthat/test-countpercluster.R
@@ -5,7 +5,7 @@ data("skin_tcell", package = "czplots")
 
 
 test_that("no error in fitting countpercluster for the skin_tcell data", {
-fit <- countpercluster(data=skin_tcell,group="tissue",plot=TRUE,plot.cluster="C0|C8")
+fit <- countpercluster(data=skin_tcell,group="tissue",level=unique(skin_tcell@meta.data$tissue))
  expect_true(inherits(fit,c("matrix","array")))
 
 })
